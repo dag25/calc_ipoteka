39d2218591584e1d9708d75a76897d6b64627d58
dag25 <dag25@mail.ru>
2024-01-04 20:35:13 +0400

    second commit

---
+++ js/view/updateResultsView.js
@@ -0,0 +1,14 @@
+import { priceFormatter, priceFormatterDecimals } from './../utils/formatters.js';
+
+function updateResultsView(results) {
+	document.querySelector('#total-percent').innerHTML = results.rate * 100 + '%';
+	document.querySelector('#total-month-payment').innerHTML = priceFormatterDecimals.format(
+		results.monthPayment,
+	);
+	document.querySelector('#total-cost').innerHTML = priceFormatter.format(results.totalAmount);
+	document.querySelector('#total-overpayment').innerHTML = priceFormatter.format(
+		results.overPayment,
+	);
+}
+
+export default updateResultsView;
---
+++ js/view/utils.js
@@ -0,0 +1,4 @@
+function updateMinPercents(data) {
+	document.querySelector('#percents-from').innerText = data.minPaymentPercents * 100 + '%';
+}
+export { updateMinPercents };
